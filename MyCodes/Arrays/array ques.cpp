86f7aaff15e847b8e17071bdcfa3fd6d4d60e9e4
DevanshuV25 <97956986+DevanshuV25@users.noreply.github.com>
2023-04-23 21:44:34 +0530

    arrays 23/04

--- MyCodes/Arrays/array ques.cpp
+++ MyCodes/Arrays/array ques.cpp
@@ -31,3 +31,64 @@ int findKthLargest(vector<int>& nums, int k)
     int ans = arr[sz-k];
     return ans;
 }
+
+void sort012(int a[], int n)
+{
+    // code here
+    int z=0,o=0,t=0;
+    for(int i=0; i<n;i++)
+    {
+        if(a[i] == 0)
+            z++;
+        if(a[i] == 1)
+            o++;
+        if(a[i] == 2)
+            t++;
+    }
+    
+    for(int i=0;i<n;i++)
+    {
+        if(z-->0)
+            a[i]=0;
+        else if(o-->0)
+            a[i]=1;
+        else if(t-->0)
+            a[i]=2;
+    }
+}
+
+void mergearrays(vector<int>& nums1, int m, vector<int>& nums2, int n)
+{
+    int l = m+n;
+    int i=0;
+    int j=0;
+    
+    vector<int> vec;
+    while(l--)
+    {
+        if(i < m && j < n){
+        if(nums1[i]<=nums2[j])
+        {
+            vec.push_back(nums1[i]);
+            i++;
+        }
+        else
+        {
+            vec.push_back(nums2[j]);
+            j++;
+        }
+    }
+        if(j == n && i < m)
+        {
+            vec.push_back(nums1[i]);
+            i++;
+        }
+        if(i == m && j < n)
+        {
+            vec.push_back(nums2[j]);
+            j++;
+        }
+    }
+    nums1 = vec;
+    
+}
--- MyCodes/Arrays/array ques.hpp
+++ MyCodes/Arrays/array ques.hpp
@@ -7,8 +7,10 @@
 // Leetcode
 // 1929. Concatenation of Array (getConcatenation)
 // 215. Kth Largest Element in an Array (findKthLarges)
+// GFG Sort an array of 0s, 1s and 2s (sort012)
+// 88. Merge Sorted Array (mergearrays)
 //
-//
+
 
 #ifndef array_ques_hpp
 #define array_ques_hpp
@@ -21,5 +23,10 @@ using namespace std;
 vector<int> getConcatenation(vector<int>& nums);
 
 int findKthLargest(vector<int>& nums, int k);
+
+void sort012(int a[], int n);
+
+void mergearrays(vector<int>& nums1, int m, vector<int>& nums2, int n);
+
 
 #endif /* array_ques_hpp */
